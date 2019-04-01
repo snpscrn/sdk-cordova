097251bd2670a41f7f3610fc75037220ef91527d
Martin Reichart <martin.reichart@caseapps.at>
2019-04-01 22:29:02 +0200

    Updated logging and documentation

--- README.md
+++ README.md
@@ -28,6 +28,28 @@ When the user cancels clip sharing the successCallback will receive an object as
 
 When the user selects a clip for sharing the successCallback will receive an object as parameter that contains the key **result** with value *share* and the keys **videoPlayerURL** and **thumbnailURL**.
 
+
+### Passing configuration parameters
+
+To pass configuration parameters, use the third parameter of the startClipSharing method to pass in a map with the following possible parameters:
+
+* *tutorialLogoImage*: Asset name for a tutorial logo image - will be loaded with [UIImage imageNamed: tutorialLogoImage];
+* *maximumTutorialLogoImageHeight*: Number with maximum tutorial logo image height in points
+* *largeSponsorImage*: Asset name for a large sponsor image image - will be loaded with [UIImage imageNamed: tutorialLogoImage];
+* *smallSponsorImage*: Asset name for a small sponsor image - will be loaded with [UIImage imageNamed: tutorialLogoImage];
+* *tutorialBackgroundImage*: Asset name for a tutorial background image - will be loaded with [UIImage imageNamed: tutorialLogoImage];
+* *snapButtonImage*: Asset name for a snap button image - will be loaded with [UIImage imageNamed: tutorialLogoImage];
+* *snapButtonTitle*: Title for the snap button
+* *sharingIntroductionHintImage*: Asset name for a sharing introduction hint image - will be loaded with [UIImage imageNamed: tutorialLogoImage];
+* *maximumSponsorImageHeight*: Number with maximum sponsor image image height in points
+* *maximumSmallSponsorImageHeight*: Number with maximum small sponsor image height in points
+* *tutorialContent*: An array of tutorial content entries with each being a map that contains *image* (asset name for an image for that tutorial page) and *text*
+* *mainButtonColor*: Main Button color in hex (without the leading #)
+* *snapViewFinderTintColor*: Snap View Finder color in hex (without the leading #)
+* *mainButtonTextColor*: Main Button Text Color in Hex (without the leading #)
+* *tutorialTextColor*: Tutorial text color in Hex (without the leading #)
+
+
 ## Support
 
 In case of any questions or problems please contact us at [support@snapscreen.com](mailto:support@snapscreen.com).
--- src/ios/Snapscreen.m
+++ src/ios/Snapscreen.m
@@ -2,7 +2,7 @@
 #import <SnapscreenKit/SnapscreenKit.h>
 #import <Cordova/CDVViewController.h>
 
-@interface Snapscreen()<SnapscreenClipSharingDelegate>
+@interface Snapscreen()<SnapscreenClipSharingDelegate, SnapscreenKitDelegate>
 @property (nonatomic, copy, nullable) NSString* callbackId;
 @end
 
@@ -12,7 +12,27 @@
     NSString* clientID = [[[self commandDelegate] settings] objectForKey: @"snapscreen-client-id"];
     NSString* secret = [[[self commandDelegate] settings] objectForKey: @"snapscreen-secret"];
     BOOL connectToTestEnvironment = [[[[self commandDelegate] settings] objectForKey: @"snapscreen-test-environment"] boolValue];
-    [SnapscreenKit sharedSnapscreenKitWithClientID: clientID clientSecret: secret testEnvironment: connectToTestEnvironment loggingHandler: nil locationProvider: nil delegate: nil];
+    id<SnapscreenLoggingHandler> loggingHandler = nil;
+#ifdef DEBUG
+    loggingHandler = [SnapscreenNSLogLoggingHandler new];
+#endif
+    [SnapscreenKit sharedSnapscreenKitWithClientID: clientID clientSecret: secret testEnvironment: connectToTestEnvironment loggingHandler: loggingHandler locationProvider: nil delegate: self];
+}
+
+- (void)snapscreenKitDidFailWithError:(NSError *)error {
+    NSLog(@"SnapscreenKit initialization did fail with error %@", error);
+}
+
+-  (UIColor *) snapscreenColorFromHexString:(NSString *)hex {
+    if (hex == nil) {
+        return nil;
+    }
+    
+    NSScanner *scanner = [NSScanner scannerWithString:hex];
+    unsigned int tempInt;
+    [scanner scanHexInt:&tempInt];
+    
+    return [UIColor colorWithRed:((tempInt>>16)&0xFF)/255.0 green:((tempInt>>8)&0xFF)/255.0    blue:(tempInt&0xFF)/255.0    alpha:1.0];
 }
 
 - (void)startClipSharing:(CDVInvokedUrlCommand*)command {
@@ -21,11 +41,88 @@
         NSData* data = [configurationData dataUsingEncoding:NSUTF8StringEncoding];
         NSDictionary* configurationDictionary = [NSJSONSerialization JSONObjectWithData: data options:NSJSONReadingAllowFragments error: nil];
         if (configurationDictionary) {
-            
-            
             self.callbackId = command.callbackId;
             
             SnapscreenClipSharingConfiguration* configuration = [SnapscreenClipSharingConfiguration new];
+            
+            NSString* tutorialLogoImageName = [configurationDictionary objectForKey: @"tutorialLogoImage"];
+            if (tutorialLogoImageName) {
+                configuration.tutorialLogoImage = [UIImage imageNamed: tutorialLogoImageName];
+            }
+            NSNumber* maximumTutorialLogoImageHeight = [configurationDictionary objectForKey: @"maximumTutorialLogoImageHeight"];
+            if (maximumTutorialLogoImageHeight) {
+                configuration.maximumTutorialLogoImageHeight = [maximumTutorialLogoImageHeight floatValue];
+            }
+            NSString* largeSponsorImageName = [configurationDictionary objectForKey: @"largeSponsorImage"];
+            if (largeSponsorImageName) {
+                configuration.largeSponsorImage = [UIImage imageNamed: largeSponsorImageName];
+            }
+            NSString* smallSponsorImageName = [configurationDictionary objectForKey: @"smallSponsorImage"];
+            if (smallSponsorImageName) {
+                configuration.smallSponsorImage = [UIImage imageNamed: smallSponsorImageName];
+            }
+            NSString* tutorialBackgroundImageName = [configurationDictionary objectForKey: @"tutorialBackgroundImage"];
+            if (tutorialBackgroundImageName) {
+                configuration.tutorialBackgroundImage = [UIImage imageNamed: tutorialBackgroundImageName];
+            }
+            NSString* snapButtonImageName = [configurationDictionary objectForKey: @"snapButtonImage"];
+            if (snapButtonImageName) {
+                configuration.snapButtonImage = [UIImage imageNamed: snapButtonImageName];
+            }
+            NSString* snapButtonTitle = [configurationDictionary objectForKey: @"snapButtonTitle"];
+            if (snapButtonTitle) {
+                configuration.snapButtonTitle = snapButtonTitle;
+            }
+            NSString* sharingIntroductionHintImageName = [configurationDictionary objectForKey: @"sharingIntroductionHintImage"];
+            if (sharingIntroductionHintImageName) {
+                configuration.sharingIntroductionHintImage = [UIImage imageNamed: sharingIntroductionHintImageName];
+            }
+            NSNumber* maximumSponsorImageHeight = [configurationDictionary objectForKey: @"maximumSponsorImageHeight"];
+            if (maximumSponsorImageHeight) {
+                configuration.maximumSponsorImageHeight = [maximumSponsorImageHeight floatValue];
+            }
+            NSNumber* maximumSmallSponsorImageHeight = [configurationDictionary objectForKey: @"maximumSmallSponsorImageHeight"];
+            if (maximumSmallSponsorImageHeight) {
+                configuration.maximumSmallSponsorImageHeight = [maximumSmallSponsorImageHeight floatValue];
+            }
+            
+            NSArray* tutorialContent = [configurationDictionary objectForKey: @"tutorialContent"];
+            if (tutorialContent) {
+                NSMutableArray* tutorialContentArray = [NSMutableArray new];
+                for (NSObject* content in tutorialContent) {
+                    if ([content isKindOfClass: [NSDictionary class]]) {
+                        NSDictionary* contentDictionary = (NSDictionary*) content;
+                        NSString* imageName = [contentDictionary objectForKey: @"image"];
+                        NSString* text = [contentDictionary objectForKey: @"text"];
+                        if (imageName) {
+                            UIImage* tutorialImage = [UIImage imageNamed: imageName];
+                            if (tutorialImage && text) {
+                                [tutorialContentArray addObject: [SnapscreenClipSharingTutorialContent contentWithImage: tutorialImage text: text]];
+                            }
+                        }
+                    }
+                }
+            }
+            
+            NSString* mainButtonColorHex = [configurationDictionary objectForKey: @"mainButtonColor"];
+            if (mainButtonColorHex) {
+                configuration.mainButtonColor = [self snapscreenColorFromHexString: mainButtonColorHex];
+            }
+            
+            NSString* snapViewFinderTintColorHex = [configurationDictionary objectForKey: @"snapViewFinderTintColor"];
+            if (snapViewFinderTintColorHex) {
+                configuration.snapViewFinderTintColor = [self snapscreenColorFromHexString: snapViewFinderTintColorHex];
+            }
+            
+            NSString* mainButtonTextColorHex = [configurationDictionary objectForKey: @"mainButtonTextColor"];
+            if (mainButtonTextColorHex) {
+                configuration.mainButtonTextColor = [self snapscreenColorFromHexString: mainButtonTextColorHex];
+            }
+            
+            NSString* tutorialTextColorHex = [configurationDictionary objectForKey: @"tutorialTextColor"];
+            if (tutorialTextColorHex) {
+                configuration.tutorialTextColor = [self snapscreenColorFromHexString: tutorialTextColorHex];
+            }
             
             SnapscreenClipSharingNavigationController* sharingNavigationController = [[SnapscreenClipSharingNavigationController alloc] initWithConfiguration: configuration delegate: self];
             [self.viewController presentViewController: sharingNavigationController animated: YES completion: nil];
